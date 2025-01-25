64ce006bb04ca9b562b1e8b9c6faf88067b7d052
ansaf-k <ansafk369@gmail.com>
2025-01-26 00:05:05 +0530

    hero

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -2,17 +2,21 @@ import "../style/Header.css"
 
 const HeroSection = () => {
     return (
-        <div className="hero w-full h-screen bg-black">
-                {/* 1st */}
-                <div className="h-screen w-1/2 bg-beige">
-                        <div className="flex justify-center items-center h-full">
-                            <img className="h-[40vh] w-[40vh] object-cover" src="../src/assets/IMG_3604.JPG" alt="" />
-                        </div>
+        <div className="hero flex w-full h-screen bg-black">
+            
+            {/* 1st */}
+            <div className="h-screen w-1/2 bg-black">
+                <div className="flex justify-center items-center h-full">
+                    <h1 className="text-5xl text-white font-bold">Welcome to my portfolio</h1>
                 </div>
-                {/* 2nd */}
-                <div className="h-screen w-1/2 bg-black">
-                   
+            </div>
+
+            {/* 2nd */}
+            <div className="h-screen w-1/2 bg-beige">
+                <div className="flex justify-center items-center h-full">
+                    <img className="h-[50vh] w-[50vh] object-cover" src="../src/assets/IMG_3604.JPG" alt="" />
                 </div>
+            </div>
         </div>
     )
 }
